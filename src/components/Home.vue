22c389f88c10440e2f95112f73bac2abe86bb7b7
didiermunezer38 <didiermunezer38@gmail.com>
2021-01-13 12:28:52 +0200

    adding index css file for all and few designs on home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,22 @@
 <template>
 <div>
     <div>
-        <div>
+        <div class="head">
             <input type="text" placeholder="Search Lessons">
         </div>
         <div>
-            <button>New Lesson</button>
+            <button class="submit">New Lesson</button>
         </div>
     </div>
-    <div>
-        <div class="recommendations"></div>
-        <div class="testimonials"></div>
+    <div class="main">
+    <div class="big">
+        <div class="recommendations">hello</div>
+        <div class="testimonials">dsfa</div>
     </div>
-    <div>
-        <div class="profile"></div>
-        <div class="stats"></div>
+    <div class="medium">
+        <div class="profile">fdsafds</div>
+        <div class="stats">dfdasdfads</div>
+    </div>
     </div>
 </div>    
 </template>
@@ -24,5 +26,40 @@ export default {
 }
 </script>
 <style scoped>
-
+.main{
+    width: 100%;
+    display: flex;
+}
+.big{
+    min-width: 69%;
+    max-width: 70%;
+    display: flex;
+}
+.big div{
+    min-width: 48%;
+    margin: 4px;
+    height: 550px;
+}
+.medium{
+    display: flex;
+    flex-direction: column;
+    min-width: 29%;
+    max-width: 30%;
+}
+.recommendations{
+    background-color: #3423;
+}
+.testimonials{
+    background-color: #3423;
+}
+.stats{
+    min-width: 90%;
+    background-color: #74B49B;
+    min-height: 40%;
+}
+.profile{
+    min-width: 90%;
+    background-color: #B0EACD;
+    min-height: 40%;
+}
 </style>
